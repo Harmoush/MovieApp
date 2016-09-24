7fc98bdb4277037f783e10721cf277c8bf53308a
Harmoush <as.harmoush@gmail.com>
2016-09-23 18:24:34 -0700

    Add files via upload

---
+++ MovieApp/app/src/main/java/com/mal/android/movieapp/instances/Review.java
@@ -0,0 +1,31 @@
+package com.mal.android.movieapp.instances;
+
+/**
+ * Created by ahmed on 9/21/2016.
+ */
+
+public class Review {
+    private String author;
+    private String content;
+
+    public Review(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
